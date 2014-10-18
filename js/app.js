99d2f479bd06efdbf91693c563ea5c1606f09743
cabechler <cathybechler@gmail.com>
2014-10-18 11:47:16 -0400

    updated js, html & css

--- js/app.js
+++ js/app.js
@@ -1,31 +1,36 @@
 
 $(document).ready(function() {
 
-var i = document.getElementById("number_entered")
+//get the number on enter
+$(".number").keypress(function(e) {
+		if (e.which == 13) {
+			var input = $("input[name=number]").val();
+			var i;
+
 
 //loop 1-100
-for (var i = 1; i <= 100; i++) {
+			for (var i = 1; i <= input && i <= 100; i++) {
 
 //create if divisible by 3 and 5 output "fizzbuzz"
-if (i % 3 === 0 && i % 5 === 0) {
-	console.log("fizzbuzz");
-}
+			if (i % 3 === 0 && i % 5 === 0) {
+			$("#fizzbuzznumbers").append("<p>fizzbuzz</p>");
+			}
 
 //create if divisible by 3 output "fizz"
-else if (i % 3 === 0) {
-	console.log("fizz");
-}
+			else if (i % 3 === 0) {
+			$("#fizzbuzznumbers").append("<p>fizz</p>");
+			}
 
 //create if divisible by 5 output = "buzz"
-else if (i % 5 === 0) {
-	console.log("buzz");
-}
+			else if (i % 5 === 0) {
+			$("#fizzbuzznumbers").append("<p>buzz</p>");
+			}
 
-//or else console.log the number
-else {
-	console.log(i);
-	}
+//or else output the number
+			else {
+			$("#fizzbuzznumbers").append("<p>" + i + "</p>");
 
-//display output
-}
-}
+			}
+		}}
+	});
+});
